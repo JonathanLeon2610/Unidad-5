23c58add3f86ce978f622bfad2c1c006ef0a222e
JonathanLeon2610 <jonathan2610leon@gmail.com>
2022-10-18 10:00:38 -0600

    Implementa el controlador UserController

---
+++ programacionavanzadaIDSTM2022-main/resources/views/create_user.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post"action="http://127.0.0.1:8000/users">
+
+        <label for="">Email</label>
+        <input type="text">
+        <label for="">Contraseña</label>
+        <input type="password">
+
+        <button>
+            Guardar
+        </button>
+    </form>
+</body>
+</html>
